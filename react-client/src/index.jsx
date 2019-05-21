7f8d3d02d8f102c6062a6bba32012bbbf6cfc5f9
alexG <alejandro.gonzalez.barriga@gmail.com>
2019-05-21 16:55:57 -0500

    created the movieRow component

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,34 +2,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import List from './components/List.jsx';
+import MovieRow from './components/MovieRow.js'
+
+
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: []
-    }
-  }
+  constructor(props){
+    super(props)
+    console.log("this is my int");
 
-  componentDidMount() {
-    $.ajax({
-      url: '/items',
-      success: (data) => {
-        this.setState({
-          items: data
-        })
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
-    });
-  }
+    const movies = [
+      {id: 0, poster_src:"https://image.tmdb.org/t/p/w185_and_h278_bestv2/or06FN3Dka5tukK1e9sl16pB3iy.jpg",
+       title: "Avengers", overview:"this is the avengers overview"},
+      {id: 1, poster_src:"https://image.tmdb.org/t/p/w185_and_h278_bestv2/cezWGskPY5x7GaglTTRN4Fugfb8.jpg",
+       title: "Avengers2", overview:"this is the second overview"}
+    ]
 
+    var movieRows = []
+    movies.forEach((movie)=>{
+      console.log(movie.title);
+      const movieRow = <MovieRow movie={movie}/>
+      movieRows.push(movieRow)
+    })
+
+    this.state = {rows:movieRows}
+};
   render () {
-    return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
+    return (
+      <div>
+        <table style={{
+          backgroundColor: '#000', display: 'block'
+        }}>
+          <tbody>
+            <tr>
+              <td>
+                <img alt="app icon" width="100" src="tmdb logo.png" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <input style={{
+          fontSize:24,
+          display: 'block',
+          width: '97.8%',
+          paddingTop: 8,
+          paddingBottom: 8,
+          paddingLeft: 16
+
+        }} placeholder = "Eneter search term"/>
+
+        {this.state.rows}
+
     </div>)
   }
 }
